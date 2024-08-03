f1db6bd456b29440954a50d8d50beea39494e95e
Ari Budimn <rhyman18@gmail.com>
2024-08-04 03:52:17 +0700

    feat(printers): create validation request for endpoint post printer

--- app/controllers/printer.js
+++ app/controllers/printer.js
@@ -1,6 +1,17 @@
 const responseJson = require("../utils/response");
+const Joi = require("joi");
 const db = require("../models");
 const Printer = db.printer;
+
+const createPrinterSchema = Joi.object({
+  name: Joi.string().min(3).max(50).required().messages({
+    "string.base": "Name must be a string",
+    "string.empty": "Name cannot be empty",
+    "string.min": "Name must be at least 1 character long",
+    "string.max": "Name must be at most 50 characters long",
+    "any.required": "Name is required",
+  }),
+});
 
 const PrinterController = {
   async findAll(req, res) {
@@ -30,6 +41,12 @@ const PrinterController = {
 
   async create(req, res) {
     try {
+      const { error } = createPrinterSchema.validate(req.body);
+
+      if (error) {
+        return responseJson(res, 400, `Failed: ${error.details[0].message}`);
+      }
+
       const createPrinter = await Printer.create({
         name: req.body.name,
       });
