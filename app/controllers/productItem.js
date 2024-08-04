bd359524f86ee0400c9e089697214a7f4413e0f3
Ari Budimn <rhyman18@gmail.com>
2024-08-04 23:13:25 +0700

    feat(productItems): create endpoint get list product items

--- app/controllers/productItem.js
+++ app/controllers/productItem.js
@@ -1,5 +1,23 @@
+const responseJson = require("../utils/response");
+const db = require("../models");
+const ProductItem = db.productItem;
+
 const ProductItemController = {
-  async findAll(req, res) {},
+  async findAll(req, res) {
+    try {
+      const getProductItems = await ProductItem.findAll({
+        include: [{ model: db.productCategory }],
+      });
+
+      if (!getProductItems.length) {
+        return responseJson(res, 400, "Failed: Product items is empty");
+      }
+
+      return responseJson(res, 200, "Success", getProductItems);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
 };
 
 module.exports = ProductItemController;
--- app/router/productItem.js
+++ app/router/productItem.js
@@ -1,4 +1,54 @@
 const express = require("express");
 const productItemRoute = express.Router();
+const ProductItemController = require("../controllers/productItem");
+
+/**
+ * @swagger
+ * /api/product/items:
+ *   get:
+ *     summary: Get List Product Items
+ *     description: To get a list of all product items
+ *     tags:
+ *       - Product Items
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: Jeruk
+ *                       productCategoryId:
+ *                         type: integer
+ *                         example: 1
+ *                       productCategory:
+ *                         type: object
+ *                         properties:
+ *                           id:
+ *                             type: integer
+ *                             example: 1
+ *                           name:
+ *                             type: string
+ *                             example: Minuman
+ *       400:
+ *         description: Failed Invalid message
+ */
+productItemRoute.get("/product/items", (req, res) => {
+  ProductItemController.findAll(req, res);
+});
 
 module.exports = productItemRoute;
