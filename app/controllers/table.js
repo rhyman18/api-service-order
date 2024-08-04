414c5305bfef8770400d7b3ceba99031d81161d3
Ari Budimn <rhyman18@gmail.com>
2024-08-04 19:08:16 +0700

    feat(tables): create endpoint get list tables

--- app/controllers/table.js
+++ app/controllers/table.js
@@ -1,5 +1,21 @@
+const responseJson = require("../utils/response");
+const db = require("../models");
+const Table = db.table;
+
 const TableController = {
-  async findAll(req, res) {},
+  async findAll(req, res) {
+    try {
+      const getTables = await Table.findAll();
+
+      if (!getTables.length) {
+        return responseJson(res, 400, "Failed: Tables is empty");
+      }
+
+      return responseJson(res, 200, "Success", getTables);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
 };
 
 module.exports = TableController;
--- app/router/table.js
+++ app/router/table.js
@@ -1,4 +1,42 @@
 const express = require("express");
 const tableRoute = express.Router();
+const TableController = require("../controllers/table");
+
+/**
+ * @swagger
+ * /api/tables:
+ *   get:
+ *     summary: Get List Tables
+ *     description: To get a list of all tables
+ *     tags:
+ *       - Tables
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: MEJA NO 1
+ *       400:
+ *         description: Failed Invalid message
+ */
+tableRoute.get("/tables", (req, res) => {
+  TableController.findAll(req, res);
+});
 
 module.exports = tableRoute;
