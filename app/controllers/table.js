37a76fa70a1c7832a47b67dda3c7300434993fba
Ari Budimn <rhyman18@gmail.com>
2024-08-04 19:13:32 +0700

    feat(tables): create endpoint get table by id

--- app/controllers/table.js
+++ app/controllers/table.js
@@ -16,6 +16,22 @@ const TableController = {
       return responseJson(res, 400, `Failed: ${error}`);
     }
   },
+
+  async findOne(req, res) {
+    try {
+      const getTable = await Table.findOne({
+        where: { id: req.params.id },
+      });
+
+      if (!getTable) {
+        return responseJson(res, 404, "Failed: Table not found");
+      }
+
+      return responseJson(res, 200, "Success", getTable);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
 };
 
 module.exports = TableController;
--- app/router/table.js
+++ app/router/table.js
@@ -39,4 +39,48 @@ tableRoute.get("/tables", (req, res) => {
   TableController.findAll(req, res);
 });
 
+/**
+ * @swagger
+ * /api/tables/{id}:
+ *   get:
+ *     summary: Get Info Table
+ *     description: To get a list of one Table based on id
+ *     tags:
+ *       - Tables
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The table ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 1
+ *                     name:
+ *                       type: string
+ *                       example: MEJA NO 1
+ *       400:
+ *         description: Failed Invalid message
+ *       404:
+ *         description: Failed Table not found
+ */
+tableRoute.get("/tables/:id", (req, res) => {
+  TableController.findOne(req, res);
+});
+
 module.exports = tableRoute;
