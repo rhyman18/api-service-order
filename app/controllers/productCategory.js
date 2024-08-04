d8ca6c3f0b56f39572a8bb2771780e56efb7b2c3
Ari Budimn <rhyman18@gmail.com>
2024-08-04 20:33:02 +0700

    feat(productCategories): create endpoint get list product category

--- app/controllers/productCategory.js
+++ app/controllers/productCategory.js
@@ -1,5 +1,21 @@
+const responseJson = require("../utils/response");
+const db = require("../models");
+const ProductCategory = db.productCategory;
+
 const ProductCategoryController = {
-  async findAll(req, res),
+  async findAll(req, res) {
+    try {
+      const getProductCategory = await ProductCategory.findAll();
+
+      if (!getProductCategory.length) {
+        return responseJson(res, 400, "Failed: Product categories is empty");
+      }
+
+      return responseJson(res, 200, "Success", getProductCategory);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
 };
 
 module.exports = ProductCategoryController;
--- app/router/productCategory.js
+++ app/router/productCategory.js
@@ -1,4 +1,42 @@
 const express = require("express");
 const productCategoryRoute = express.Router();
+const ProductCategoryController = require("../controllers/productCategory");
 
-module.exports  = productCategoryRoute;
+/**
+ * @swagger
+ * /api/product/categories:
+ *   get:
+ *     summary: Get List Product Categories
+ *     description: To get a list of all product categories
+ *     tags:
+ *       - Product Categories
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: Minuman
+ *       400:
+ *         description: Failed Invalid message
+ */
+productCategoryRoute.get("/product/categories", (req, res) => {
+  ProductCategoryController.findAll(req, res);
+});
+
+module.exports = productCategoryRoute;
