8282bd8e4bbb6580fcd9c6c022a5e46d6377fdad
Ari Budimn <rhyman18@gmail.com>
2024-08-05 20:49:31 +0700

    feat(productVariants): craete endpoint get list product variants

--- app/controllers/productVariant.js
+++ app/controllers/productVariant.js
@@ -1,5 +1,24 @@
+const responseJson = require("../utils/response");
+const db = require("../models");
+const ProductVariant = db.productVariant;
+const ProductItem = db.productItem;
+
 const ProductVariantController = {
-  async findAll(req, res) {},
+  async findAll(req, res) {
+    try {
+      const getProductVariants = await ProductVariant.findAll({
+        include: [{ model: ProductItem }],
+      });
+
+      if (!getProductVariants.length) {
+        return responseJson(res, 400, "Failed: Product variants is empty");
+      }
+
+      return responseJson(res, 200, "Success", getProductVariants);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
 };
 
 module.exports = ProductVariantController;
--- app/router/productVariant.js
+++ app/router/productVariant.js
@@ -1,4 +1,60 @@
 const express = require("express");
 const productVariantRoute = express.Router();
+const ProductVariantController = require("../controllers/productVariant");
+
+/**
+ * @swagger
+ * /api/product/variants:
+ *   get:
+ *     summary: Get List Product Variants
+ *     description: To get a list of all product variants
+ *     tags:
+ *       - Product Variants
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: DINGIN
+ *                       price:
+ *                         type: float
+ *                         example: 12000
+ *                       productItemId:
+ *                         type: integer
+ *                         example: 1
+ *                       productItem:
+ *                         type: object
+ *                         properties:
+ *                           id:
+ *                             type: integer
+ *                             example: 1
+ *                           name:
+ *                             type: string
+ *                             example: Jeruk
+ *                           productCategoryId:
+ *                             type: integer
+ *                             example: 1
+ *       400:
+ *         description: Failed Invalid message
+ */
+productVariantRoute.get("/product/variants", (req, res) => {
+  ProductVariantController.findAll(req, res);
+});
 
 module.exports = productVariantRoute;
