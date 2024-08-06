c1b7fc1466e7813b170d33eb7b6a31542e057ce8
Ari Budimn <rhyman18@gmail.com>
2024-08-06 18:59:25 +0700

    refactor(validation): move validate product item to validate utils

--- app/utils/validate.js
+++ app/utils/validate.js
@@ -30,8 +30,24 @@ const productCategorySchema = Joi.object({
   }),
 });
 
+const productItemSchema = Joi.object({
+  name: Joi.string().min(3).max(50).required().messages({
+    "string.base": "Name must be a string",
+    "string.empty": "Name cannot be empty",
+    "string.min": "Name must be at least 3 character long",
+    "string.max": "Name must be at most 50 characters long",
+    "any.required": "Name is required",
+  }),
+  productCategoryId: Joi.number().required().messages({
+    "number.base": "Product Category Id must be an integer",
+    "number.empty": "Product Category Id cannot be empty",
+    "any.required": "Product Category is required",
+  }),
+});
+
 module.exports = {
   printerSchema,
   tableSchema,
   productCategorySchema,
+  productItemSchema,
 };
