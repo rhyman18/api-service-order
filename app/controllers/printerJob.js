4c30b00ff4c88b3f58ae4dcd81ce3575b4191039
Ari Budimn <rhyman18@gmail.com>
2024-08-06 23:49:40 +0700

    feat(printerJobs): create endpoint get list printer jobs

--- app/controllers/printerJob.js
+++ app/controllers/printerJob.js
@@ -1,5 +1,25 @@
+const responseJson = require("../utils/response");
+const db = require("../models");
+const PrinterJob = db.printerJob;
+const Printer = db.printer;
+const ProductCategory = db.productCategory;
+
 const PrinterJobController = {
-  async findAll(req, res) {},
+  async findAll(req, res) {
+    try {
+      const getPrinterJobs = await PrinterJob.findAll({
+        include: [{ model: Printer }, { model: ProductCategory }],
+      });
+
+      if (!getPrinterJobs.length) {
+        return responseJson(res, 400, "Failed: Printer Jobs is empty");
+      }
+
+      return responseJson(res, 200, "Success", getPrinterJobs);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
 };
 
 module.exports = PrinterJobController;
--- app/router/printerJob.js
+++ app/router/printerJob.js
@@ -1,4 +1,63 @@
 const express = require("express");
 const printerJobRoute = express.Router();
+const PrinterJobController = require("../controllers/printerJob");
+
+/**
+ * @swagger
+ * /api/printer/jobs:
+ *   get:
+ *     summary: Get List Printer Jobs
+ *     description: To get a list of all printer jobs
+ *     tags:
+ *       - Printer Jobs
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       printerId:
+ *                         type: integer
+ *                         example: 1
+ *                       productCategoryId:
+ *                         type: integer
+ *                         example: 1
+ *                       printer:
+ *                         type: object
+ *                         properties:
+ *                           id:
+ *                             type: integer
+ *                             example: 1
+ *                           name:
+ *                             type: string
+ *                             example: Printer Kasir
+ *                       productCategory:
+ *                         type: object
+ *                         properties:
+ *                           id:
+ *                             type: integer
+ *                             example: 1
+ *                           name:
+ *                             type: string
+ *                             example: Minuman
+ *       400:
+ *         description: Failed Invalid message
+ */
+printerJobRoute.get("/printer/jobs", (req, res) => {
+  PrinterJobController.findAll(req, res);
+});
 
 module.exports = printerJobRoute;
