b6463ed0289c5e4a393085596ba5688e878ac337
Ari Budimn <rhyman18@gmail.com>
2024-08-06 22:42:32 +0700

    chore(swagger): create tags description with sorted tags

---
+++ app/utils/tags.js
@@ -0,0 +1,32 @@
+const tagDocs = [
+  {
+    name: "Check",
+    description: "API checking",
+  },
+  {
+    name: "Products",
+    description: "API detailed Products",
+  },
+  {
+    name: "Product Categories",
+    description: "CRUD Product categories",
+  },
+  {
+    name: "Product Items",
+    description: "CRUD Product items",
+  },
+  {
+    name: "Product Variants",
+    description: "CRUD Product Variants",
+  },
+  {
+    name: "Tables",
+    description: "CRUD Table Order",
+  },
+  {
+    name: "Printers",
+    description: "CRUD Printer Devices",
+  },
+];
+
+module.exports = tagDocs;
