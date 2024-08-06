415cbbac173fd32ce290e26be5ede06a97cd4c1d
Ari Budimn <rhyman18@gmail.com>
2024-08-06 21:54:45 +0700

    feat(products): create endpoint get list products detailed by category

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -7,7 +7,7 @@ const ProductVariant = db.productVariant;
 const ProductController = {
   async findAll(req, res) {
     try {
-      const gettProducts = await ProductItem.findAll({
+      const getProducts = await ProductItem.findAll({
         include: [
           { model: ProductCategory },
           { model: ProductVariant, attributes: { exclude: ["productItemId"] } },
@@ -15,11 +15,39 @@ const ProductController = {
         attributes: { exclude: ["productCategoryId"] },
       });
 
-      if (!gettProducts.length) {
+      if (!getProducts.length) {
         return responseJson(res, 400, "Failed: Products is empty");
       }
 
-      return responseJson(res, 200, "Success", gettProducts);
+      return responseJson(res, 200, "Success", getProducts);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
+
+  async findByCategory(req, res) {
+    try {
+      const getCategory = await ProductCategory.findOne({
+        where: { name: req.params.category },
+      });
+
+      if (!getCategory) {
+        return responseJson(res, 400, "Failed: Products category not found");
+      }
+
+      const getProducts = await ProductItem.findAll({
+        where: { productCategoryId: getCategory.id },
+        include: [
+          { model: ProductVariant, attributes: { exclude: ["productItemId"] } },
+        ],
+        attributes: { exclude: ["productCategoryId"] },
+      });
+
+      if (!getProducts.length) {
+        return responseJson(res, 400, "Failed: Products is empty");
+      }
+
+      return responseJson(res, 200, "Success", getProducts);
     } catch (error) {
       return responseJson(res, 400, `Failed: ${error}`);
     }
--- app/router/products.js
+++ app/router/products.js
@@ -62,4 +62,62 @@ productRoute.get("/products", (req, res) => {
   ProductController.findAll(req, res);
 });
 
+/**
+ * @swagger
+ * /api/products/{category}:
+ *   get:
+ *     summary: Get List Products by Category
+ *     description: To get a list of all products by category
+ *     tags:
+ *       - Products
+ *     parameters:
+ *       - name: category
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The product category name
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 5
+ *                       name:
+ *                         type: string
+ *                         example: Mie
+ *                       productVariants:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             id:
+ *                               type: integer
+ *                               example: 8
+ *                             name:
+ *                               type: string
+ *                               example: GORENG
+ *                             price:
+ *                               type: integer
+ *                               example: 15000
+ *       400:
+ *         description: Failed Invalid message
+ */
+productRoute.get("/products/:category", (req, res) => {
+  ProductController.findByCategory(req, res);
+});
+
 module.exports = productRoute;
