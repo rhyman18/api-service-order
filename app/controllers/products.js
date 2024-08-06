12035ca81f70ae832f18bcc4c0aa97174978839c
Ari Budimn <rhyman18@gmail.com>
2024-08-06 22:15:36 +0700

    feat(products): create endpoint get product detailed by id

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -52,6 +52,27 @@ const ProductController = {
       return responseJson(res, 400, `Failed: ${error}`);
     }
   },
+
+  async findOne(req, res) {
+    try {
+      const getProduct = await ProductItem.findOne({
+        where: { id: req.params.id },
+        include: [
+          { model: ProductCategory },
+          { model: ProductVariant, attributes: { exclude: ["productItemId"] } },
+        ],
+        attributes: { exclude: ["productCategoryId"] },
+      });
+
+      if (!getProduct) {
+        return responseJson(res, 400, "Failed: Product not found");
+      }
+
+      return responseJson(res, 200, "Success", getProduct);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
 };
 
 module.exports = ProductController;
--- app/router/products.js
+++ app/router/products.js
@@ -121,4 +121,71 @@ productRoute.get("/products/:category", (req, res) => {
   ProductController.findByCategory(req, res);
 });
 
+/**
+ * @swagger
+ * /api/products/id/{id}:
+ *   get:
+ *     summary: Get Info Product
+ *     description: To get a product detail
+ *     tags:
+ *       - Products
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The product ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: Jeruk
+ *                       productCategory:
+ *                         type: object
+ *                         properties:
+ *                           id:
+ *                             type: integer
+ *                             example: 1
+ *                           name:
+ *                             type: string
+ *                             example: Minuman
+ *                       productVariants:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             id:
+ *                               type: integer
+ *                               example: 1
+ *                             name:
+ *                               type: string
+ *                               example: DINGIN
+ *                             price:
+ *                               type: integer
+ *                               example: 12000
+ *       400:
+ *         description: Failed Invalid message
+ */
+productRoute.get("/products/id/:id", (req, res) => {
+  ProductController.findOne(req, res);
+});
+
 module.exports = productRoute;
