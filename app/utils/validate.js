dfc843bb56e0e1666ffff0cfe3ea4fc63d2a4da6
Ari Budimn <rhyman18@gmail.com>
2024-08-06 19:14:55 +0700

    refactor(validation): move validate product variant to validate utils

--- app/utils/validate.js
+++ app/utils/validate.js
@@ -45,9 +45,82 @@ const productItemSchema = Joi.object({
   }),
 });
 
+const productVariantSchema = {
+  create() {
+    return Joi.object({
+      productItemId: Joi.number().integer().required().messages({
+        "number.base": "Product Item Id must be an integer",
+        "any.required": "Product Item Id is required",
+      }),
+      variants: Joi.array()
+        .items(
+          Joi.object({
+            name: Joi.string().min(3).max(50).required().messages({
+              "string.base": "Name must be a string",
+              "string.empty": "Name cannot be empty",
+              "string.min": "Name must be at least 1 character long",
+              "string.max": "Name must be at most 50 characters long",
+              "any.required": "Name is required",
+            }),
+            price: Joi.number().precision(2).required().messages({
+              "number.base": "Price must be a float",
+              "any.required": "Price is required",
+            }),
+          })
+        )
+        .min(1)
+        .required()
+        .messages({
+          "array.base": "Variants must be an array",
+          "array.min": "Variants array cannot be empty",
+          "any.required": "Variants are required",
+        }),
+    });
+  },
+
+  update() {
+    return Joi.object({
+      name: Joi.string().min(3).max(50).required().messages({
+        "string.base": "Name must be a string",
+        "string.empty": "Name cannot be empty",
+        "string.min": "Name must be at least 1 character long",
+        "string.max": "Name must be at most 50 characters long",
+        "any.required": "Name is required",
+      }),
+      price: Joi.number().precision(2).required().messages({
+        "number.base": "Price must be a float",
+        "any.required": "Price is required",
+      }),
+    });
+  },
+
+  createWithout() {
+    return Joi.object({
+      price: Joi.number().precision(2).required().messages({
+        "number.base": "Price must be a float",
+        "any.required": "Price is required",
+      }),
+      productItemId: Joi.number().integer().required().messages({
+        "number.base": "Product Item Id must be an integer",
+        "any.required": "Product Item Id is required",
+      }),
+    });
+  },
+
+  without() {
+    return Joi.object({
+      price: Joi.number().precision(2).required().messages({
+        "number.base": "Price must be a float",
+        "any.required": "Price is required",
+      }),
+    });
+  },
+};
+
 module.exports = {
   printerSchema,
   tableSchema,
   productCategorySchema,
   productItemSchema,
+  productVariantSchema,
 };
