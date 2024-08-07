1806593bbf79044cbaaba2ce13a9160be4ffdc3c
Ari Budimn <rhyman18@gmail.com>
2024-08-08 00:35:05 +0700

    feat(validation): typo of warning min strig

--- app/utils/validate.js
+++ app/utils/validate.js
@@ -58,7 +58,7 @@ const productVariantSchema = {
             name: Joi.string().min(3).max(50).required().messages({
               "string.base": "Name must be a string",
               "string.empty": "Name cannot be empty",
-              "string.min": "Name must be at least 1 character long",
+              "string.min": "Name must be at least 3 character long",
               "string.max": "Name must be at most 50 characters long",
               "any.required": "Name is required",
             }),
@@ -83,7 +83,7 @@ const productVariantSchema = {
       name: Joi.string().min(3).max(50).required().messages({
         "string.base": "Name must be a string",
         "string.empty": "Name cannot be empty",
-        "string.min": "Name must be at least 1 character long",
+        "string.min": "Name must be at least 3 character long",
         "string.max": "Name must be at most 50 characters long",
         "any.required": "Name is required",
       }),
