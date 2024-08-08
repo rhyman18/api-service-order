0fcdff90cf7058f4ecb682b4e06e6a7900cf3571
Ari Budimn <rhyman18@gmail.com>
2024-08-08 22:19:12 +0700

    feat(bill): create endpoint get list bill

--- app/controllers/bill.js
+++ app/controllers/bill.js
@@ -1,5 +1,80 @@
+const responseJson = require("../utils/response");
+const db = require("../models");
+const {
+  order: Order,
+  orderProduct: OrderProduct,
+  productVariant: ProductVariant,
+  productItem: ProductItem,
+  productCategory: ProductCategory,
+  table: Table,
+} = db;
+
 const BillController = {
-  async findAll(req, res) {},
+  async findAll(req, res) {
+    try {
+      const getBills = await Order.findAll({
+        include: [
+          { model: Table },
+          {
+            model: OrderProduct,
+            include: [
+              {
+                model: ProductVariant,
+                include: [
+                  {
+                    model: ProductItem,
+                    include: [
+                      {
+                        model: ProductCategory,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      });
+
+      if (!getBills.length) {
+        return responseJson(res, 400, "Failed: Bills is empty");
+      }
+
+      const detailBills = getBills.map((bill) => {
+        const {
+          id,
+          customerName,
+          paymentMethod,
+          table: { name: tableName },
+          orderProducts,
+          createdAt,
+        } = bill;
+
+        const products = orderProducts.map((orderProduct) => ({
+          category:
+            orderProduct.productVariant.productItem.productCategory.name,
+          name: orderProduct.productVariant.productItem.name,
+          variant: orderProduct.productVariant.name,
+          price: orderProduct.productVariant.price,
+          quantity: orderProduct.quantity,
+          total: orderProduct.productVariant.price * orderProduct.quantity,
+        }));
+
+        return {
+          id,
+          customerName,
+          paymentMethod,
+          tableName,
+          products,
+          date: createdAt,
+        };
+      });
+
+      return responseJson(res, 200, "Success", detailBills);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error.message}`);
+    }
+  },
 };
 
 module.exports = BillController;
--- app/router/bill.js
+++ app/router/bill.js
@@ -1,4 +1,75 @@
 const express = require("express");
 const billRoute = express.Router();
+const BillController = require("../controllers/bill");
+
+/**
+ * @swagger
+ * /api/bills:
+ *   get:
+ *     summary: Get List of Bills
+ *     description: Retrieve a list of all bills with detailed information about each bill and its products.
+ *     tags:
+ *       - Bills
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 28
+ *                       customerName:
+ *                         type: string
+ *                         example: John Doe
+ *                       paymentMethod:
+ *                         type: string
+ *                         example: Credit Card
+ *                       tableName:
+ *                         type: string
+ *                         example: MEJA NO 1
+ *                       products:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             category:
+ *                               type: string
+ *                               example: Makanan
+ *                             name:
+ *                               type: string
+ *                               example: Mie
+ *                             variant:
+ *                               type: string
+ *                               example: GORENG
+ *                             price:
+ *                               type: integer
+ *                               example: 15000
+ *                             quantity:
+ *                               type: integer
+ *                               example: 1
+ *                             total:
+ *                               type: integer
+ *                               example: 15000
+ *                       date:
+ *                         type: string
+ *                         format: date-time
+ *                         example: 2024-08-07T17:16:15.000Z
+ *       400:
+ *         description: Failed Invalid message
+ */
+billRoute.get("/bills", (req, res) => {
+  BillController.findAll(req, res);
+});
 
 module.exports = billRoute;
