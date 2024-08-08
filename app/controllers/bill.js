4cf2cfb7ffd8a562fb0c13c59232d388b96cd296
Ari Budimn <rhyman18@gmail.com>
2024-08-08 23:33:49 +0700

    feat(bill): crate endpoint get bill by date range

--- app/controllers/bill.js
+++ app/controllers/bill.js
@@ -1,5 +1,6 @@
 const responseJson = require("../utils/response");
 const db = require("../models");
+const { Op } = db.Sequelize;
 const {
   order: Order,
   orderProduct: OrderProduct,
@@ -137,6 +138,85 @@ const BillController = {
       };
 
       return responseJson(res, 200, "Success", detailBill);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error.message}`);
+    }
+  },
+
+  async findByDate(req, res) {
+    try {
+      const { start, end } = req.query;
+
+      let dateFilter = {};
+      if (start && end) {
+        dateFilter = {
+          createdAt: {
+            [Op.between]: [new Date(start), new Date(end)],
+          },
+        };
+      }
+
+      const getBills = await Order.findAll({
+        where: dateFilter,
+        include: [
+          { model: Table },
+          {
+            model: OrderProduct,
+            include: [
+              {
+                model: ProductVariant,
+                include: [
+                  {
+                    model: ProductItem,
+                    include: [
+                      {
+                        model: ProductCategory,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      });
+
+      if (!getBills.length) {
+        return responseJson(res, 400, "Failed: Bills are empty");
+      }
+
+      const detailBills = getBills.map((bill) => {
+        const {
+          id,
+          customerName,
+          paymentMethod,
+          table: { name: tableName },
+          orderProducts,
+          createdAt,
+        } = bill;
+
+        const products = orderProducts.map((orderProduct) => ({
+          category:
+            orderProduct.productVariant.productItem.productCategory.name,
+          name: orderProduct.productVariant.productItem.name,
+          variant: orderProduct.productVariant.name,
+          price: orderProduct.productVariant.price,
+          quantity: orderProduct.quantity,
+          total: orderProduct.productVariant.price * orderProduct.quantity,
+        }));
+
+        return {
+          id,
+          customerName,
+          paymentMethod,
+          tableName,
+          products,
+          grandTotal: products.reduce((sum, product) => sum + product.total, 0),
+          date: createdAt,
+        };
+      });
+
+      return responseJson(res, 200, "Success", detailBills);
     } catch (error) {
       return responseJson(res, 400, `Failed: ${error.message}`);
     }
--- app/router/bill.js
+++ app/router/bill.js
@@ -77,6 +77,94 @@ billRoute.get("/bills", (req, res) => {
 
 /**
  * @swagger
+ * /api/bills/date:
+ *   get:
+ *     summary: Get List of Bills by Date range
+ *     description: Retrieve a list of all bills with detailed information about each bill and its products. Optionally, filter bills by date range.
+ *     tags:
+ *       - Bills
+ *     parameters:
+ *       - name: start
+ *         in: query
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *           example: 2024-08-05 00:00:00
+ *         description: Start date for filtering bills
+ *       - name: end
+ *         in: query
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *           example: 2024-08-10 00:00:00
+ *         description: End date for filtering bills
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       customerName:
+ *                         type: string
+ *                         example: John Doe
+ *                       paymentMethod:
+ *                         type: string
+ *                         example: Credit Card
+ *                       tableName:
+ *                         type: string
+ *                         example: MEJA NO 1
+ *                       products:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             category:
+ *                               type: string
+ *                               example: Minuman
+ *                             name:
+ *                               type: string
+ *                               example: Jeruk
+ *                             variant:
+ *                               type: string
+ *                               example: DINGIN
+ *                             price:
+ *                               type: integer
+ *                               example: 12000
+ *                             quantity:
+ *                               type: integer
+ *                               example: 1
+ *                             total:
+ *                               type: integer
+ *                               example: 12000
+ *                       grandTotal:
+ *                         type: integer
+ *                         example: 87000
+ *                       date:
+ *                         type: string
+ *                         format: date-time
+ *                         example: 2024-08-07T17:16:15.000Z
+ *       400:
+ *         description: Failed Invalid message
+ */
+billRoute.get("/bills/date", (req, res) => {
+  BillController.findByDate(req, res);
+});
+
+/**
+ * @swagger
  * /api/bills/{id}:
  *   get:
  *     summary: Get Bill Details
