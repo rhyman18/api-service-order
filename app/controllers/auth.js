4d6738fb9e1e4bfa4dd2c7f1af0780cb9e34e5a8
Ari Budimn <rhyman18@gmail.com>
2024-08-09 20:30:50 +0700

    feat(auth): create endpoint post signin

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const responseJson = require("../utils/response");
-const { signUpSchema } = require("../utils/validate");
+const { signUpSchema, signInSchema } = require("../utils/validate");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const db = require("../models");
 const User = db.user;
 
@@ -30,6 +32,50 @@ const AuthController = {
         name,
         email,
         password,
+      };
+
+      return responseJson(res, 200, "Success", response);
+    } catch (error) {
+      return responseJson(res, 400, `Failed: ${error}`);
+    }
+  },
+
+  async signIn(req, res) {
+    try {
+      const { error } = signInSchema.validate(req.body);
+      if (error) {
+        return responseJson(res, 400, `Failed: ${error.details[0].message}`);
+      }
+
+      const { email, password } = req.body;
+
+      const existingUser = await User.findOne({ where: { email } });
+      if (!existingUser) {
+        return responseJson(res, 400, "Failed: Email not found");
+      }
+
+      const verifyPassword = bcrypt.compareSync(
+        password,
+        existingUser.password
+      );
+      if (!verifyPassword) {
+        return responseJson(res, 400, "Failed: Wrong password");
+      }
+
+      const generateToken = jwt.sign(
+        {
+          id: existingUser.id,
+        },
+        process.env.SECRET_AUTH,
+        {
+          expiresIn: 86400,
+        }
+      );
+
+      const response = {
+        name: existingUser.name,
+        email,
+        accessToken: generateToken,
       };
 
       return responseJson(res, 200, "Success", response);
