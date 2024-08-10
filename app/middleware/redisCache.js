55ffa51c60f28be23efb6e21f119586eca670d9a
Ari Budimn <rhyman18@gmail.com>
2024-08-11 02:45:58 +0700

    feat(redis): add retry connect strategy

--- app/middleware/redisCache.js
+++ app/middleware/redisCache.js
@@ -3,9 +3,31 @@ const Redis = require("ioredis");
 
 class RedisCache {
   constructor() {
+    this.maxRetries = 3;
+    this.retryDelay = 3000;
+    this.retryCount = 0;
+
     this.redisClient = new Redis({
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
+      retryStrategy: (times) => {
+        if (this.retryCount >= this.maxRetries) {
+          return null;
+        }
+
+        this.retryCount++;
+        const delay = this.retryDelay;
+        return delay;
+      },
+      reconnectOnError: (err) => {
+        const targetError = "READONLY";
+        if (err.message.includes(targetError)) {
+          return true;
+        }
+        return false;
+      },
+      maxRetriesPerRequest: 0,
+      enableReadyCheck: true,
     });
 
     this.redisClient.on("error", (err) => {
@@ -13,33 +35,52 @@ class RedisCache {
     });
 
     this.redisClient.on("connect", () => {
-      console.log("Connected to Redis");
+      console.log("Redis Connected");
+      this.retryCount = 0;
     });
 
     this.redisClient.on("close", () => {
-      console.log("Disconnected from Redis");
+      console.log("Redis Disconnected");
     });
   }
 
   async get(key) {
-    return await this.redisClient.get(key);
+    try {
+      return await this.redisClient.get(key);
+    } catch (err) {
+      console.error("Redis get error:", err.message);
+      return null;
+    }
   }
 
   async set(key, value) {
-    await this.redisClient.set(
-      key,
-      JSON.stringify(value),
-      "EX",
-      process.env.REDIS_EXPIRE
-    );
+    try {
+      await this.redisClient.set(
+        key,
+        JSON.stringify(value),
+        "EX",
+        process.env.REDIS_EXPIRE
+      );
+    } catch (err) {
+      console.error("Redis set error:", err.message);
+    }
   }
 
   async del(key) {
-    await this.redisClient.del(key);
+    try {
+      await this.redisClient.del(key);
+    } catch (err) {
+      console.error("Redis del error:", err.message);
+    }
   }
 
   async keys(key) {
-    return await this.redisClient.keys(key);
+    try {
+      return await this.redisClient.keys(key);
+    } catch (err) {
+      console.error("Redis keys error:", err.message);
+      return [];
+    }
   }
 
   status() {
